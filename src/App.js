df51b4332e096c46f3e6f3c6c96f4f123ab01c05
caterpillar <stm7798@gmail.com>
2024-09-17 22:01:34 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import WeatherApp from "./WeatherApp";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello from App</header>
-      <WeatherApp />
+      <div className="container">
+        <header className="App-header">hello from App</header>
+        <WeatherApp />
+      </div>
     </div>
   );
 }
