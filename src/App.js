b97099127047fe72201c80aec7bfdd2a542a9d5d
caterpillar <stm7798@gmail.com>
2024-09-22 21:45:13 +0330

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import WeatherApp from "./WeatherApp";
+import "./App.css";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <WeatherApp defaultCity="New York" />
+        <footer className="footer">
+          This project was coded by caterpillarCodes and is{" "}
+          <a
+            href="https://github.com/caterpillarCodes/weather-application-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on Vercel
+        </footer>
       </div>
     </div>
   );
